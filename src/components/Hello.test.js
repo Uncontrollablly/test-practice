a3f028e706a3a25459bdacdfb9999f64d12166be
yuqi.wang <yuqi.wang@thoughtworks.com>
2021-05-26 17:06:02 +0800

    feat: add Hello component and some tests

---
+++ src/components/Hello.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import Hello from "./Hello";
+
+describe("it should show name", () => {
+  test("it should show default message if have name prop", () => {
+    // given pass name prop
+    const name = "xxx";
+    // when render finished
+    render(<Hello name={name}></Hello>);
+    // then there should show '你好，{props.name}！'
+    expect(
+      screen.getByText(`你好，${name}`, { exact: false })
+    ).toBeInTheDocument();
+  });
+
+  test("it should show default message if not have name prop", () => {
+    // given not pass name prop
+    // when render finished
+    render(<Hello></Hello>);
+    // then there should show '嘿，陌生人'
+    expect(screen.getByText("嘿，陌生人")).toBeInTheDocument();
+  });
+});
